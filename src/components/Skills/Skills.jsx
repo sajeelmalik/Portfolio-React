d9fe8b4ae347d009ac8760503680df9d628a8025
Sajeel Malik <sajeel23@berkeley.edu>
2019-05-20 13:15:28 -0700

    introduce material kit components for Skills section

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -10,6 +10,7 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import BeachAccessIcon from '@material-ui/icons/BeachAccess';
+import NavPills from "components/NavPills/NavPills.jsx";
 
 
 import ShareIcon from '@material-ui/icons/Share';
